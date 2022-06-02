d8c2eeffe9ae5d65d243fd474fb695e92075f23d
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-02 08:49:45 -0300

    Update

---
+++ JDBC/src/jdbc/TesteConexao.java
@@ -0,0 +1,23 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+	
+	public static void main(String[] args) throws SQLException{
+		
+		String url = "jdbc:mysql://localhost:";
+		final String usuario = "root";
+		final String senha = "159753864a";
+		
+		Connection conexao =
+				DriverManager.getConnection(url, usuario, senha);
+		
+		System.out.println("Conexão Efetuada Com Sucesso!");
+		conexao.close();		
+		
+	}
+
+}
