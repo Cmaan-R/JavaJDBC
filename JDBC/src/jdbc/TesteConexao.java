d6c975408754ce4cd7788f72a95e022dc4f93c10
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-02 10:54:25 -0300

    Update

--- JDBC/src/jdbc/TesteConexao.java
+++ JDBC/src/jdbc/TesteConexao.java
@@ -8,7 +8,7 @@ public class TesteConexao {
 	
 	public static void main(String[] args) throws SQLException{
 		
-		final String url = "jdbc:mysql://localhost:";
+		final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
 		final String usuario = "root";
 		final String senha = "159753864a";
 		
