7af70445ea2ecf4c665a51df51a60f2730672547
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-02 20:29:40 -0300

    Update

--- JDBC/src/jdbc/DesafioAtualizarRegistro.java
+++ JDBC/src/jdbc/DesafioAtualizarRegistro.java
@@ -17,11 +17,11 @@ public class DesafioAtualizarRegistro {
 		System.out.println("Informe o ID: ");
 		int codigo = entrada.nextInt();
 		
-		String select = "SELECT codigo, nome FROM pessoas WHERE codigo = ?";
-		String update = "Update pessoas SET nome = ? WHERE codigo = ?";
+		String selectSQL = "SELECT codigo, nome FROM pessoas WHERE codigo = ?";
+		String updateSQL = "Update pessoas SET nome = ? WHERE codigo = ?";
 		
 		Connection conexao = FabricaConexao.getConexao();
-		PreparedStatement stmt = conexao.prepareStatement(select);
+		PreparedStatement stmt = conexao.prepareStatement(selectSQL);
 		stmt.setInt(1, codigo);
 		ResultSet resultado = stmt.executeQuery();
 		
@@ -29,19 +29,21 @@ public class DesafioAtualizarRegistro {
 			Pessoa p = new Pessoa(resultado.getInt(1), resultado.getString(2));
 			System.out.println("O nome atual é " + p.getNome());
 			entrada.nextLine();
-		}
 		
 		System.out.println("Informe Um Novo Nome Para o Registro: ");
 		String nome = entrada.nextLine();
 	
 		stmt.close();
-		stmt = conexao.prepareStatement(update);
+		
+		stmt = conexao.prepareStatement(updateSQL);
 		stmt.setString(1, nome);
 		stmt.setInt(2, codigo);
 		stmt.execute();
 		
 		System.out.println("Pessoa Alterada Com Sucesso");
-		 
+	} else {
+		System.out.println("Pessoa Não Encontrada");	 
+	}
 		conexao.close();
 		entrada.close();
 	}
